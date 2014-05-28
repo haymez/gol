e6e19210a3d4e15a34b4d97936f22c102dde9b4f
James Miltenberger <jamilt13@gmail.com>
2014-05-28 17:46:53 -0500

    Updated style.. Somewhat

--- js/gol.js
+++ js/gol.js
@@ -1,6 +1,6 @@
 /*
  * This file contains all the JavaScript necessary to run this game of life app
- * author: James Miltenberger 
+ * Author: James Miltenberger 
  */
 
 /* newGame - Initializes a new game of life
